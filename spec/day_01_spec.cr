f557658ecb42f88f6ea0b87409f2b47e2580474c
Jason Schweier <jason.schweier@gmail.com>
2021-12-01 18:41:07 -0500

    Day 1, part 2

--- spec/day_01_spec.cr
+++ spec/day_01_spec.cr
@@ -19,3 +19,19 @@ end
 it "solves part 1" do
   count_increases(Input.integers(1)).should eq(1754)
 end
+
+describe "#count_increases_sliding_window" do
+  it "is zero without enough elements" do
+    count_increases_sliding_window([1]).should eq(0)
+    count_increases_sliding_window([1,2,3]).should eq(0)
+  end
+
+  it "compares sliding window of three elements" do
+    count_increases_sliding_window([1,2,3,4]).should eq(1)
+    count_increases_sliding_window([1,2,3,4,5,6]).should eq(3)
+  end
+end
+
+it "solves part 2" do
+  count_increases_sliding_window(Input.integers(1)).should eq(1789)
+end
