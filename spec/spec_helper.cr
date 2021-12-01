d1f2a65b016e0987c96e1c4d5ff021b32c9db08b
Jason Schweier <jason.schweier@gmail.com>
2021-12-01 18:27:20 -0500

    Add helper to parse input to array of integers

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,6 +1,16 @@
 require "spec"
 
 module Input
-  def numbers(day)
+  extend self
+
+  def integers(day : Int)
+    File
+      .read(Path[Dir.current].join("data", normalize_day(day)))
+      .split("\n")
+      .map(&.to_i)
+  end
+
+  def normalize_day(day : Int)
+    day.to_s.rjust(2, '0')
   end
 end
