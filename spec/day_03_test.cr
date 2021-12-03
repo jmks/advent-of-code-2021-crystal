402c0be14378cc1274af8b7373eab1f4aff49033
Jason Schweier <jason.schweier@gmail.com>
2021-12-03 08:45:02 -0500

    Day 3, part 1

---
+++ spec/day_03_test.cr
@@ -0,0 +1,46 @@
+require "./spec_helper"
+
+require "../src/day_03"
+
+describe "gamma_rate" do
+  it "is the most common bit of each input" do
+    column_frequency(["010", "101", "010"]).should eq([
+      {"0" => 2, "1" => 1},
+      {"0" => 1, "1" => 2},
+      {"0" => 2, "1" => 1},
+    ])
+  end
+end
+
+describe "gamma_rate" do
+  it "is the most common bit of each input" do
+    gamma_rate([
+                 {"0" => 2, "1" => 1},
+                 {"0" => 1, "1" => 2},
+                 {"0" => 2, "1" => 1},
+               ]).should eq(2)
+  end
+end
+
+describe "power_consumption" do
+  it "calculates example" do
+    power_consumption([
+                        "00100",
+                        "11110",
+                        "10110",
+                        "10111",
+                        "10101",
+                        "01111",
+                        "00111",
+                        "11100",
+                        "10000",
+                        "11001",
+                        "00010",
+                        "01010",
+                      ]).should eq(198)
+  end
+end
+
+it "solves part 1" do
+  power_consumption(Input.strings(3)).should eq(12)
+end
