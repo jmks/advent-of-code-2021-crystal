024848bd71daeb0a232d415c49b3feeb159887b5
Jason Schweier <jason.schweier@gmail.com>
2021-12-07 09:04:04 -0500

    Day 7, part 2

--- spec/day_07_spec.cr
+++ spec/day_07_spec.cr
@@ -5,8 +5,16 @@ describe "#minimum_fuel" do
   it "bruteforces the example" do
     minimum_fuel([16,1,2,0,4,2,7,1,2,14]).should eq(37)
   end
+
+  it "calculates with cumlative cost function" do
+    minimum_fuel([16,1,2,0,4,2,7,1,2,14], :cumulative).should eq(168)
+  end
 end
 
 it "solves part 1" do
   minimum_fuel(Input.arrays_of_integers(7).first).should eq(352_707)
+end
+
+it "solves part 2" do
+  minimum_fuel(Input.arrays_of_integers(7).first, :cumulative).should eq(95_519_693)
 end
--- src/day_07.cr
+++ src/day_07.cr
@@ -31,13 +31,36 @@
 
 # Determine the horizontal position that the crabs can align to using the least fuel possible. How much fuel must they spend to align to that position?
 
-def minimum_fuel(crabs : Array(Int32))
+# --- Part Two ---
+
+# The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab engineering?
+
+# As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the second step costs 2, the third step costs 3, and so on.
+
+# As each crab moves, moving further becomes more expensive. This changes the best horizontal position to align them all on; in the example above, this becomes 5:
+
+# Move from 16 to 5: 66 fuel
+# Move from 1 to 5: 10 fuel
+# Move from 2 to 5: 6 fuel
+# Move from 0 to 5: 15 fuel
+# Move from 4 to 5: 1 fuel
+# Move from 2 to 5: 6 fuel
+# Move from 7 to 5: 3 fuel
+# Move from 1 to 5: 10 fuel
+# Move from 2 to 5: 6 fuel
+# Move from 14 to 5: 45 fuel
+
+# This costs a total of 168 fuel. This is the new cheapest possible outcome; the old alignment position (2) now costs 206 fuel instead.
+
+# Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an escape route! How much fuel must they spend to align to that position?
+
+def minimum_fuel(crabs : Array(Int32), movement_cost = :linear)
   max = crabs.max
 
   positions = Array.new(max + 1, 0)
 
   (0..max).each do |index|
-    positions[index] = crabs.map { |crab| (crab - index).abs }.sum
+    positions[index] = crabs.map { |crab| cost(crab, index, movement_cost) }.sum
   end
 
   positions.min
@@ -47,4 +70,17 @@ def parse(input : String)
   input
     .split(",", remove_empty: true)
     .map(&.to_i)
+end
+
+private def cost(crab, index, movement_cost)
+  case movement_cost
+  when :linear
+    (crab - index).abs
+  else
+    distance = (crab - index).abs
+
+    return 0 if distance == 0
+
+    (1..distance).sum
+  end
 end
