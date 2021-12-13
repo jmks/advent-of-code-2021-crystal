0a64c5834f646043a751e5cc932d03f1cb9869ac
Jason Schweier <jason.schweier@gmail.com>
2021-12-13 17:27:00 -0500

    Day 12, part 2

--- spec/day_12_spec.cr
+++ spec/day_12_spec.cr
@@ -11,14 +11,84 @@ A-end
 b-end
 ".split("\n", remove_empty: true)
 
+larger = "
+dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc
+".split("\n", remove_empty: true)
+
+big = "
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
+".split("\n", remove_empty: true)
+
+describe VisitSingleSmallCaveAtMostTwice do
+  it "can revisit a big cave" do
+    VisitSingleSmallCaveAtMostTwice.new.can_visit?(["start", "A"], CaveNode.new("A")).should be_true
+  end
+
+  it "can not revist start" do
+    VisitSingleSmallCaveAtMostTwice.new.can_visit?(["start"], CaveNode.new("start")).should be_false
+  end
+
+  it "can visit a single small cave twice" do
+    VisitSingleSmallCaveAtMostTwice.new.can_visit?(["start", "a"], CaveNode.new("a")).should be_true
+  end
+
+  it "can not visit a single small cave thrice" do
+    VisitSingleSmallCaveAtMostTwice.new.can_visit?(["start", "a", "a"], CaveNode.new("a")).should be_false
+  end
+
+  it "can not visit a small cave twice, if one has already been visited twice" do
+    VisitSingleSmallCaveAtMostTwice.new.can_visit?(["start", "a", "a", "b"], CaveNode.new("b")).should be_false
+  end
+end
+
 describe CaveMap do
   describe "#paths" do
-    it "returns all the paths" do
-      CaveMap.parse(example).paths("start", "end").size.should eq(10)
+    context "when visiting small caves only once" do
+      it "returns all the paths" do
+        CaveMap.parse(example).paths("start", "end", VisitSmallCavesOnce.new).size.should eq(10)
+      end
+
+      it "solves part 1" do
+        CaveMap.parse(Input.strings(12)).paths("start", "end", VisitSmallCavesOnce.new).size.should eq(4659)
+      end
     end
 
-    it "solves part 1" do
-      CaveMap.parse(Input.strings(12)).paths("start", "end").size.should eq(4659)
+    context "when visiting a single small cave at most twice" do
+      it "returns all the paths" do
+        CaveMap.parse(example).paths("start", "end", VisitSingleSmallCaveAtMostTwice.new).size.should eq(36)
+        CaveMap.parse(larger).paths("start", "end", VisitSingleSmallCaveAtMostTwice.new).size.should eq(103)
+        CaveMap.parse(big).paths("start", "end", VisitSingleSmallCaveAtMostTwice.new).size.should eq(3509)
+      end
+
+      it "solves part 2" do
+        CaveMap.parse(Input.strings(12)).paths("start", "end", VisitSingleSmallCaveAtMostTwice.new).size.should eq(148962)
+      end
     end
   end
 end
--- src/day_12.cr
+++ src/day_12.cr
@@ -95,6 +95,53 @@
 
 # How many paths through this cave system are there that visit small caves at most once?
 
+# --- Part Two ---
+
+# After reviewing the available paths, you realize you might have time to visit a single small cave twice. Specifically, big caves can be visited any number of times, a single small cave can be visited at most twice, and the remaining small caves can be visited at most once. However, the caves named start and end can only be visited exactly once each: once you leave the start cave, you may not return to it, and once you reach the end cave, the path must end immediately.
+
+# Now, the 36 possible paths through the first example above are:
+
+# start,A,b,A,b,A,c,A,end
+# start,A,b,A,b,A,end
+# start,A,b,A,b,end
+# start,A,b,A,c,A,b,A,end
+# start,A,b,A,c,A,b,end
+# start,A,b,A,c,A,c,A,end
+# start,A,b,A,c,A,end
+# start,A,b,A,end
+# start,A,b,d,b,A,c,A,end
+# start,A,b,d,b,A,end
+# start,A,b,d,b,end
+# start,A,b,end
+# start,A,c,A,b,A,b,A,end
+# start,A,c,A,b,A,b,end
+# start,A,c,A,b,A,c,A,end
+# start,A,c,A,b,A,end
+# start,A,c,A,b,d,b,A,end
+# start,A,c,A,b,d,b,end
+# start,A,c,A,b,end
+# start,A,c,A,c,A,b,A,end
+# start,A,c,A,c,A,b,end
+# start,A,c,A,c,A,end
+# start,A,c,A,end
+# start,A,end
+# start,b,A,b,A,c,A,end
+# start,b,A,b,A,end
+# start,b,A,b,end
+# start,b,A,c,A,b,A,end
+# start,b,A,c,A,b,end
+# start,b,A,c,A,c,A,end
+# start,b,A,c,A,end
+# start,b,A,end
+# start,b,d,b,A,c,A,end
+# start,b,d,b,A,end
+# start,b,d,b,end
+# start,b,end
+
+# The slightly larger example above now has 103 paths through it, and the even larger example now has 3509 paths through it.
+
+# Given these new rules, how many paths through this cave system are there?
+
 class CaveNode
   getter name
   getter connections
@@ -116,6 +163,25 @@ class CaveNode
 
   def big?
     !small?
+  end
+end
+
+class VisitSmallCavesOnce
+  def can_visit?(path : Array(String), cave : CaveNode)
+    cave.big? || !path.includes?(cave.name)
+  end
+end
+
+class VisitSingleSmallCaveAtMostTwice
+  def can_visit?(path : Array(String), cave : CaveNode)
+    return true if cave.big?
+    return false if cave.name == "start"
+    return true if !path.includes?(cave.name)
+
+    path
+      .tally
+      .select { |name, count| name[0, 1].downcase == name[0, 1] && count > 1 }
+      .empty?
   end
 end
 
@@ -147,7 +213,7 @@ class CaveMap
     @nodes[destination] = dest
   end
 
-  def paths(source, destination)
+  def paths(source, destination, path_rules)
     complete_paths = Array(Array(String)).new
 
     states = [{source, Array(String).new}]
@@ -159,9 +225,9 @@ class CaveMap
       if name == destination
         complete_paths << path.dup.push(destination)
       else
-        options = node.connections.select { |cave| cave.big? || !path.includes?(cave.name) }
+        options = node.connections.select { |cave| path_rules.can_visit?(path, cave) }
 
-        states.concat(options.map { |cave| {cave.name, path.dup.push(node.name)} })
+        states.concat(options.map { |cave| {cave.name, path.dup.push(cave.name)} })
       end
     end
 
